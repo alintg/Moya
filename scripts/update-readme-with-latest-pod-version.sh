e726603596bd8279b47d487d9e6fa3493e3697dd
alintg <ali.ntg3@gmail.com>
2018-02-17 16:59:23 +0200

    Push changes only if one of the readme(s) changed

--- scripts/update-readme-with-latest-pod-version.sh
+++ scripts/update-readme-with-latest-pod-version.sh
@@ -20,8 +20,8 @@ else
 	## Update pod 'Moya/ReactiveSwift'
 	sed -i -e "s#pod 'Moya/ReactiveSwift'.*#pod 'Moya/ReactiveSwift' '~> ${version}'#" *.md
 
-	number_of_changed_readme_files=git status | grep 'Readme' | wc -l
-	if [[ number_of_changed_readme_files > 0 ]]; then
+	number_of_changed_readme_files=$(git status | grep 'Readme' | wc -l)
+	if [[ $number_of_changed_readme_files > 0 ]]; then
 		# Push changes
 		echo "Pushing chnages to Readme(s)."
 		git config credential.helper 'cache --timeout=120'
@@ -32,6 +32,6 @@ else
 		# Push quitely to prevent showing the token in log
 		git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/alintg/Moya.git update-pod-install-in-readme-ci
 	fi
-	
+
 	echo "Done."
 fi
